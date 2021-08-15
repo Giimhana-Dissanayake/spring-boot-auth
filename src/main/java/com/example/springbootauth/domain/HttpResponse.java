6a476f773559fc07f1c1f484cd510dd4093d208c
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-15 21:24:45 +0530

    36.test endpoint for security

--- src/main/java/com/example/springbootauth/domain/HttpResponse.java
+++ src/main/java/com/example/springbootauth/domain/HttpResponse.java
@@ -1,9 +1,14 @@
 package com.example.springbootauth.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import org.springframework.http.HttpStatus;
+
+import java.util.Date;
 
 public class HttpResponse {
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "MM-dd-yyyy hh:mm:ss", timezone = "Asia/Colombo")
+    private Date timeStamp;
     private int httpStatusCode;
     private HttpStatus httpStatus;
     private String reason;
@@ -14,6 +19,7 @@ public class HttpResponse {
     }
 
     public HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {
+        this.timeStamp = new Date();
         this.httpStatusCode = httpStatusCode;
         this.httpStatus = httpStatus;
         this.reason = reason;
@@ -23,6 +29,14 @@ public class HttpResponse {
     public int getHttpStatusCode() {
 
         return httpStatusCode;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
     }
 
     public void setHttpStatusCode(int httpStatusCode) {
