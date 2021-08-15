9c708e2c0cacfb927c433372b5f963d0999755dd
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-15 17:12:03 +0530

    28.JWT token provider - part 3

--- src/main/java/com/example/springbootauth/utility/JWTTokenProvider.java
+++ src/main/java/com/example/springbootauth/utility/JWTTokenProvider.java
@@ -10,21 +10,29 @@ import static java.util.Arrays.stream;
 
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.example.springbootauth.domain.UserPrincipal;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 
+import javax.servlet.http.HttpServletRequest;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class JWTTokenProvider {
 
     @Value("${jwt.secret}")
     private String secret;
 
+    //big method
     public String generateJwtToken(UserPrincipal userPrincipal) {
         String[] claims = getClaimsFromUser(userPrincipal);
         return JWT.create().withIssuer(GET_ARRAYS_LLC)
@@ -34,6 +42,7 @@ public class JWTTokenProvider {
                 .sign(HMAC512(secret.getBytes(StandardCharsets.UTF_8)));
     }
 
+    //big method
     public List<GrantedAuthority> getAuthorities(String token) {
         String[] claims = getClaimsFromToken(token);
         return stream(claims).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
@@ -53,6 +62,32 @@ public class JWTTokenProvider {
             throw new JWTVerificationException(TOKEN_CANNOT_BE_VERIFIED);
         }
         return verifier;
+    }
+
+    //checkout vedio 28 to know what this does
+    //big method
+    public Authentication getAuthentication(String username, List<GrantedAuthority> authorities, HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+                new UsernamePasswordAuthenticationToken(username, null, authorities);
+        usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        return usernamePasswordAuthenticationToken;
+    }
+
+    //big method
+    public boolean isTokenValid(String username, String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return StringUtils.isNotBlank(username) && !isTokenExpired(verifier, token);
+    }
+
+    //big method
+    public String getSubject(String token) {
+        JWTVerifier verifier = getJWTVerifier();
+        return verifier.verify(token).getSubject();
+    }
+
+    private boolean isTokenExpired(JWTVerifier verifier, String token) {
+        Date expiration = verifier.verify(token).getExpiresAt();
+        return expiration.before(new Date());
     }
 
     private String[] getClaimsFromUser(UserPrincipal userPrincipal) {
