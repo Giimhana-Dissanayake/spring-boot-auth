895b60b9300a5112bf308662508fa503bc0e0572
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-15 23:56:30 +0530

    43.override spring default white label error

--- src/main/java/com/example/springbootauth/exception/domain/ExceptionHandling.java
+++ src/main/java/com/example/springbootauth/exception/domain/ExceptionHandling.java
@@ -15,6 +15,7 @@ import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import javax.persistence.NoResultException;
 import java.io.IOException;
--- src/main/java/com/example/springbootauth/resource/UserResource.java
+++ src/main/java/com/example/springbootauth/resource/UserResource.java
@@ -1,18 +1,22 @@
 package com.example.springbootauth.resource;
 
 import com.example.springbootauth.domain.User;
+import com.example.springbootauth.exception.domain.EmailExistException;
 import com.example.springbootauth.exception.domain.ExceptionHandling;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/user")
+@RequestMapping(path = {"/", "/user"})
 public class UserResource extends ExceptionHandling {
 
     @GetMapping("/home")
     public String showUser(){
         return "app works!!";
     }
+
+
+
 
 }
