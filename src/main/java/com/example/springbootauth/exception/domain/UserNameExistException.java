ae03234a2c304c0bfce73e73b86f4da707f5e422
Gimhana Dissanayake <gimhanadev@gmail.com>
2021-08-15 22:17:40 +0530

    38.custom exception class

---
+++ src/main/java/com/example/springbootauth/exception/domain/UserNameExistException.java
@@ -0,0 +1,7 @@
+package com.example.springbootauth.exception.domain;
+
+public class UserNameExistException extends Exception{
+    public UserNameExistException(String message) {
+        super(message);
+    }
+}
