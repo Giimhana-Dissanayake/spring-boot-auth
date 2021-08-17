d5fe51681bf20d671e220eda9c306b52f6d1b528
Gimhana-Dissanayake <gimhanadev@gmail.com>
2021-08-18 00:47:40 +0530

    49.User registration part 3

--- src/main/java/com/example/springbootauth/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/springbootauth/service/impl/UserServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.springbootauth.exception.domain.EmailExistException;
 import com.example.springbootauth.exception.domain.UserNameExistException;
 import com.example.springbootauth.repository.UserRepository;
 import com.example.springbootauth.service.UserService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,11 +15,15 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.transaction.Transactional;
 import java.util.Date;
 import java.util.List;
+
+import static com.example.springbootauth.enumaration.Role.ROLE_USER;
 
 @Service
 @Transactional
@@ -27,10 +32,12 @@ public class UserServiceImpl implements UserService, UserDetailsService {
 
     private Logger LOGGER = LoggerFactory.getLogger(getClass());
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
+    public UserServiceImpl(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -54,7 +61,39 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     @Override
     public User register(String firstName, String lastName, String userName, String email) throws EmailExistException, UserNameExistException {
         validateNewUserNameAndEmail(StringUtils.EMPTY, userName, email);
+        User user = new User();
+        user.setUserId(generateUserId());
+        String password = generatePassword();
+        String encodedPassword = encodePassword(password);
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        user.setEmail(email);
+        user.setJoinDate(new Date());
+        user.setPassword(encodedPassword);
+        user.setActive(true);
+        user.setNotLocked(true);
+        user.setRole(ROLE_USER.name());
+        user.setAuthorities(ROLE_USER.getAuthorities());
+        user.setProfileImageUrl(getTemporaryProfileImageUrl());
+        userRepository.save(user);
+        LOGGER.info("New user password: " + password);
         return null;
+    }
+
+    private String getTemporaryProfileImageUrl() {
+        return ServletUriComponentsBuilder.fromCurrentContextPath().path("/user/image/profile/temp").toUriString();
+    }
+
+    private String encodePassword(String password) {
+        return passwordEncoder.encode(password);
+    }
+
+    private String generatePassword() {
+        return RandomStringUtils.randomAlphanumeric(10);
+    }
+
+    private String generateUserId() {
+        return RandomStringUtils.randomNumeric(10);
     }
 
     private User validateNewUserNameAndEmail(String currentUserName, String newUserName, String newEmail) throws
